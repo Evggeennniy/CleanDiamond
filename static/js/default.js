3476270e3bbcf0613eb739c8936307133f6e4aee
Evgeniy <myiphone128@mail.ru>
2025-03-12 19:42:39 +0200

    fixs

--- static/js/default.js
+++ static/js/default.js
@@ -18,3 +18,22 @@ window.addEventListener("resize", () => {
     navbarModal.classList.remove("open");
   }
 });
+
+const modal = document.querySelector(".contact__modal_wrapper");
+const sendButton = document.querySelector(".form__bottom_btns_send");
+
+sendButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  sendButton.disabled = true;
+
+  modal.classList.add("visible");
+
+  setTimeout(() => {
+    modal.classList.remove("visible");
+
+    sendButton.disabled = false;
+
+    sendButton.closest("form").submit();
+  }, 3000);
+});
